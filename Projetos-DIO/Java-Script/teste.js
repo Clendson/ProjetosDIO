b927db7e38b755ec7dfd8cade61ff9e820473c1c
Clendson <clendson2@gmail.com>
2024-02-07 16:27:40 -0400

    Movendo codigos

---
+++ Projetos-DIO/Java-Script/teste.js
@@ -0,0 +1,7 @@
+//declarando uma Matriz
+let NomePessoa2 = [ ["Antonio Esterco","31", "Masculino"], ["Piter Parque","30","Masculino"], 
+["Capitão","33","Masculino"]]
+//imprimindo um dos dados da Matriz linha 1 e coluna 2 (idade do Antonio)
+console.log(NomePessoa2[0][0])
+
+ 
