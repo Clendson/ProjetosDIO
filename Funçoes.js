e7330f45a6ca6ad057f03341e61e742cab9366ff
Clendson <clendson2@gmail.com>
2024-02-08 10:03:50 -0400

    Adicionando Pj Funções

---
+++ Funçoes.js
@@ -0,0 +1,14 @@
+function torrar(){
+
+    console.log ("Torrando Pão")
+}
+
+function ColocarPao(){
+    console.log("Preparando para colocar o pão!");
+    console.log("Finalizando...")
+    console.log("Pronto")
+
+
+}
+torrar();
+ColocarPao();
